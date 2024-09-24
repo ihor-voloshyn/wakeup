75e48908a7a94fe9606fdb859097acfac0f8fe18
Ihor Voloshyn <testbox.ua@gmail.com>
2024-09-24 10:46:19 +0200

    commit 20240924

--- src/PreventSleep.java
+++ src/PreventSleep.java
@@ -2,15 +2,14 @@ import java.awt.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-//import java.time.format.DateTimeFormatter;
 
 public class PreventSleep {
-    //private static Robot robot;
 
     public static void main(String[] args) {
+
         System.out.println(LocalDateTime.now());
         System.out.println(ZonedDateTime.now(ZoneId.of("Europe/Kiev")).toLocalDateTime());
-        System.out.println(Long.MAX_VALUE);
+
         try {
             Robot robot = new Robot();
             int delay = 30000;
